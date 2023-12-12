2eac6aa8d05e56fe74c6360357fd5da26f5e02c4
Matthias Linhuber <matthias@linhuber.org>
2023-12-12 18:05:39 +0100

    Add initial types

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hades-result-container-test
+
+TODOs
+- Read Metadata from environment variables
+- Populate the DTO with the metadata
+- Dockerize the application
+- Make the endpoint configurable
+- Make the test results directory configurable
--- main.go
+++ main.go
@@ -4,9 +4,35 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/joshdk/go-junit"
 	log "github.com/sirupsen/logrus"
+)
+
+/**
+TODOs
+- Read Metadata from environment variables
+- Populate the DTO with the metadata
+- Dockerize the application
+- Make the endpoint configurable
+- Make the test results directory configurable
+*/
+
+type ResultMetadata struct {
+	AssignmentRepoBranchName string `json:"assignmentRepoBranchName"`
+	AssignmentRepoCommitHash string `json:"assignmentRepoCommitHash"`
+	TestsRepoCommitHash      string `json:"testsRepoCommitHash"`
+	IsBuildSuccessful        bool   `json:"isBuildSuccessful"`
+	BuildRunDate             string `json:"buildRunDate"`
+}
+type ResultDTO struct {
+	ResultMetadata
+	BuildJobs []junit.Suite `json:"buildJobs"`
+}
+
+const (
+	APIendpoint = "http://localhost:3001/result"
 )
 
 func main() {
@@ -16,7 +42,42 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	toJSON(suites)
+
+	reportResult(suites)
+}
+
+func reportResult(suites []junit.Suite) {
+	var resultDTO ResultDTO
+	for _, suite := range suites {
+		resultDTO.BuildJobs = append(resultDTO.BuildJobs, suite)
+	}
+
+	// Convert the DTO to JSON
+	jsonData, err := json.Marshal(resultDTO)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new request using http
+	req, err := http.NewRequest("POST", APIendpoint, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create a Client
+	client := &http.Client{}
+
+	// Send the request via a client
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close response body
+	defer resp.Body.Close()
 }
 
 func toJSON(suites []junit.Suite) {
